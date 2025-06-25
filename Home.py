c19e26a2fe69c192d91c5720c047c562f10fdf97
jeanmagnus <jeanjr.001@hotmail.com>
2025-06-25 16:32:56 -0300

    mod

--- Home.py
+++ Home.py
@@ -6,24 +6,14 @@ st.title("🧠 Saúde Mental no Setor de Tecnologia")
 
 
 
-if pagina == "Introdução":
-    st.markdown("""
-    Este projeto usa dados da **OSMI (Open Sourcing Mental Illness)** para analisar padrões relacionados à saúde mental entre profissionais de tecnologia.
+st.markdown("""
+Este projeto usa dados da **OSMI (Open Sourcing Mental Illness)** para analisar padrões relacionados à saúde mental entre profissionais de tecnologia.
 
-    Explore as páginas ao lado para navegar entre:
-    - 📊 Estatísticas e visualizações gerais
-    - 📈 Comparações por gênero, país, idade e trabalho remoto
-    - 🔁 Correlações entre fatores de apoio e tratamento
+Explore as páginas ao lado para navegar entre:
+- 📊 Estatísticas e visualizações gerais
+- 📈 Comparações por gênero, país, idade e trabalho remoto
+- 🔁 Correlações entre fatores de apoio e tratamento
 
-    Os dados foram obtidos do repositório [OSMI](https://osmihelp.org/research).
-    """)
-    st.image("https://miro.medium.com/v2/resize:fit:1200/1*iEzMdG7CQyLP6JAL51_X1g.png", use_column_width=True)
-
-elif pagina == "Análise Geral":
-    Analise_Geral.show()
-
-elif pagina == "Comparações":
-    Comparacoes.show()
-
-elif pagina == "Correlações":
-    Correlacoes.show()
+Os dados foram obtidos do repositório [OSMI](https://osmihelp.org/research).
+""")
+#st.image("https://miro.medium.com/v2/resize:fit:1200/1*iEzMdG7CQyLP6JAL51_X1g.png", use_column_width=True)
